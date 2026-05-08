9b02cef680120018bacc192a44e1da68eb50e730
jishnu <jishnuu160@gmail.com>
2026-05-08 11:19:09 +0530

    removed line of code

--- app.js
+++ app.js
@@ -66,12 +66,10 @@ app.use((req, res, next) => {
     res.locals.error = req.query.error || (flashError.length > 0 ? flashError : null);
     res.locals.success = req.query.success || (flashSuccess.length > 0 ? flashSuccess : null);
 
-    // 🟢 BETTER SEPARATION
-    // Only assign req.user to res.locals.user if it's NOT an admin path
     if (!req.path.startsWith('/admin')) {
         res.locals.user = req.session.user || req.user || null;
     } else {
-        res.locals.user = null; // Ensure User data doesn't bleed into Admin views
+        res.locals.user = null; 
     }
 
     res.locals.admin = req.session.admin || null; 
