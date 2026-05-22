dc760e6acdd08aede610c56bfc9cf5308c8b0d25
jishnu <jishnuu160@gmail.com>
2026-05-22 16:47:05 +0530

    added categorywith pagination

--- repositories/adminRepository.js
+++ repositories/adminRepository.js
@@ -1,4 +1,5 @@
 import Admin from '../models/adminModel.js';
+import Category from '../models/categoryModel.js'
 
 class AdminRepository {
     async findAdminByEmail(email) {
@@ -7,3 +8,12 @@ class AdminRepository {
 }
 
 export default new AdminRepository();
+
+export const findCategoriesWithPagination = async(query,skip,limit)=>{
+    return await Category.find(query).populate('parentCategory').skip(skip).limit(limit).sort({createdAt:-1}).lean();
+};
+
+export const countCategories= async(query)=>{
+    return await Category.countDocuments(query);
+};
+
