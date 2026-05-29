864683ef6d7cd00f342fd0ca28150afc54e3d34c
jishnu <jishnuu160@gmail.com>
2026-05-29 10:02:05 +0530

    otp not coming to gmail issue

--- services/userServices.js
+++ services/userServices.js
@@ -10,7 +10,6 @@ export const signup = async (data) => {
         throw new Error('User already exists');
     }
    
-
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
@@ -21,7 +20,6 @@ export const signup = async (data) => {
         otp: null,
         otpExpiry: null
     };
-
     return await userRepo.createUser(userData);
 };
 
@@ -51,7 +49,7 @@ const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: 'stephive3@gmail.com', 
-        pass: 'breq jigw tzzq xzzj'  
+        pass: 'luqf qasx eirr ggrj'  
     }
 });
 
@@ -66,7 +64,7 @@ export const sendOTP = async (email) => {
   
     // --- THIS IS WHERE YOU ADD THE LOG BACK ---
     console.log(`\n=========================================`);
-    console.log(`DEBUG: OTP for ${email} is [ ${otp} ]`);
+    console.log(` OTP for ${email} is [ ${otp} ]`);
     console.log(`=========================================\n`);
     // ------------------------------------------
 
@@ -74,7 +72,24 @@ export const sendOTP = async (email) => {
         from: '"StepHive Support" <stephive3@gmail.com>',
         to: email, 
         subject: 'StepHive - Your Verification Code',
-        html: `...`
+        html: `
+    <div style="font-family: Arial; padding:20px;">
+        <h2>StepHive Verification</h2>
+
+        <p>Your OTP code is:</p>
+
+        <h1 style="
+            letter-spacing:5px;
+            color:#2563eb;
+        ">
+            ${otp}
+        </h1>
+
+        <p>
+            This OTP will expire in 1 minute.
+        </p>
+    </div>
+`
     };
 
     try {
@@ -129,8 +144,44 @@ export const resetPassword = async (email, newPassword) => {
     return updatedUser;
 };
 
-export const addAddress = async (userId, addressData) => {
-   
 
-    return await userRepo.addAddress(userId, addressData);
+
+export const updateAvatar = async (userId, imagePath) => {
+
+    return await userRepo.updateUserInfo(userId, {
+        profileImage: imagePath
+    });
+
+};
+
+export const getUserById = async (userId) => {
+   return await userRepo.findById(userId);
+};
+
+export const updateProfile = async (userId, data) => {
+   return await userRepo.updateUserInfo(userId, data);
+};
+
+export const addAddress = async (userId, addressData) => {
+   return await userRepo.addAddress(userId, addressData);
+};
+
+export const deleteAddress = async (userId, addressId) => {
+   return await userRepo.deleteAddress(userId, addressId);
+};
+
+export const editAddress = async (
+   userId,
+   addressId,
+   updatedData
+) => {
+   return await userRepo.updateAddress(
+      userId,
+      addressId,
+      updatedData
+   );
+};
+
+export const findUserByEmail = async (email) => {
+   return await userRepo.findByEmail(email);
 };
