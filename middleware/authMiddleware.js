19d48f4e0f50eebfc3e003af46d2fe48953d53e1
jishnu <jishnuu160@gmail.com>
2026-06-05 09:57:38 +0530

    added cartmodel import added middleware to get cart items count

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,6 @@
 import User from '../models/userModel.js';
+import Cart from '../models/cartModel.js'; 
+
 
 export const isUserAuthenticated = async (req, res, next) => {
     if (req.session && req.session.user) {
@@ -67,4 +69,24 @@ export const preventCache = (req, res, next) => {
     res.setHeader('Pragma', 'no-cache');
     res.setHeader('Expires', '0');
     next();
+};
+
+
+/* ---------------- GLOBAL NAVBAR DATA MIDDLEWARE ---------------- */
+export const injectNavbarData = async (req, res, next) => {
+    try {
+        res.locals.user = req.session.user || null;
+        res.locals.cartCount = 0;
+
+        if (req.session.user) {
+            const cart = await Cart.findOne({ userId: req.session.user._id || req.session.user.id });
+            if (cart && cart.items && cart.items.length > 0) {
+                res.locals.cartCount = cart.items.reduce((total, item) => total + item.quantity, 0);
+            }
+        }
+        next();
+    } catch (error) {
+        console.error("Error generating global navbar data:", error);
+        next();
+    }
 };
