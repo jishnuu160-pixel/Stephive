287e801c6e1a77f1224297aa08e93db746af34a6
jishnu <jishnuu160@gmail.com>
2026-07-10 10:17:10 +0530

    made changes to function to get exact result

--- repositories/returnRepository.js
+++ repositories/returnRepository.js
@@ -1,25 +1,24 @@
 import Return from '../models/ReturnModel.js';
 
 export const saveReturn = async (returnData) => {
-    try {
-        const newReturn = new Return(returnData);
-        return await newReturn.save();
-    } catch (error) {
-        throw new Error(`Error saving return: ${error.message}`);
-    }
+    return await new Return(returnData).save();
 };
 
 export const findReturnById = async (id) => {
-    return await Return.findById(id).populate('orderId productId');
+    return await Return.findOne({ returnId: id }).populate('productId');
 };
 
 export const findAllReturns = async () => {
-    return await Return.find().sort({ createdAt: -1 });
+    return await Return.find()
+        .populate('userId', 'fullName email')
+        .populate('orderId', 'orderId')
+        .sort({ createdAt: -1 })
+        .lean(); 
 };
 
 export const updateReturnStatus = async (id, status) => {
-    return await Return.findByIdAndUpdate(
-        id, 
+    return await Return.findOneAndUpdate(
+        { returnId: id },
         { returnStatus: status }, 
         { new: true }
     );
