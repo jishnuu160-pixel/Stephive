d1f8adc775bb3ae329cc24e6b7d60f2eabe9122f
jishnu <jishnuu160@gmail.com>
2026-07-10 10:18:03 +0530

    added returnId generator

--- utils/idGenerator.js
+++ utils/idGenerator.js
@@ -1,10 +1,14 @@
 export const generateOrderID = () => {
-  
-    const randomNum = Math.ceil(Math.random() * 9999);
-    
-    const paddedNum = randomNum.toString().padStart(4, '0');
-    
+    const randomNum = Math.ceil(Math.random() * 9999);  
+    const paddedNum = randomNum.toString().padStart(4, '0');  
     const timestamp = Date.now().toString().slice(-4);
     
     return `ORD-${timestamp}-${paddedNum}`; 
+};
+
+export const generateReturnID = () => {
+    const randomNum = Math.ceil(Math.random() * 9999);
+    const paddedNum = randomNum.toString().padStart(4, '0');
+    const timestamp = Date.now().toString().slice(-4);
+    return `RET-${timestamp}-${paddedNum}`; 
 };
