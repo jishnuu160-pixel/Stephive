31a5f0c006fa967cf811a3fc671c4bac891b7b92
jishnu <jishnuu160@gmail.com>
2026-07-17 14:53:34 +0530

    removed returnModel and added product,order repo

--- services/returnService.js
+++ services/returnService.js
@@ -1,19 +1,23 @@
 import * as ReturnRepo from '../repositories/ReturnRepository.js';
+import * as ProductRepo from '../repositories/productRepository.js';
+import * as OrderRepo from '../repositories/orderRepository.js';
 import * as OrderService from './OrderService.js';
 import { generateReturnID } from '../utils/idGenerator.js';
-import ReturnModel from '../models/ReturnModel.js';
 
 export const processReturnRequest = async (userId, orderId, body) => {
+
     const order = await OrderService.getUserOrderDetails(orderId, userId);
-    
-    const existingReturn=await ReturnModel.findOne({orderId:orderId});
-    if(existingReturn){
+
+    const existingReturn = await ReturnRepo.findByOrderId(order.orderId);
+
+    if (existingReturn) {
         throw new Error("A return request already made for this order");
     }
-    
-    const item = order.items.find(i => 
+
+    const item = order.items.find(i =>
         i.productId._id.toString() === body.productId.toString()
     );
+
     if (!item) {
         throw new Error("The requested product was not found in this order.");
     }
@@ -26,18 +30,20 @@ export const processReturnRequest = async (userId, orderId, body) => {
         orderId: order.orderId,
         returnId: generatedId,
         productId: body.productId,
-        productName: item.productName,      
-        size: item.size,                    
-        quantity: item.quantity,            
-        productImage: item.productImage,      
+        variantId: item.variantId,
+        productName: item.productName,
+        size: item.size,
+        quantity: item.quantity,
+        productImage: item.productImage,
         returnType: body.returnType,
         reason: body.reason,
         refundMode: body.refundMode,
-        refundAmount: refundAmount, 
+        refundAmount,
         expectedPickupDate: body.pickupDate,
         pickupAddress: order.deliveryAddress,
         returnStatus: 'Pending'
     };
+
     return await ReturnRepo.saveReturn(returnData);
 };
 
@@ -52,7 +58,8 @@ export const getReturnDetailsById = async (returnId) => {
 };
 
 export const checkIfReturnExists = async (orderIdString) => {
-    const existing = await ReturnModel.findOne({ orderId: orderIdString});
-    console.log("Existing:",existing);
+    const existing = await ReturnRepo.findByOrderId(orderIdString);
+    console.log("Existing:", existing);
     return !!existing;
 };
+
