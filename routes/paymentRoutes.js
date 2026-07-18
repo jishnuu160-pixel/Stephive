feed097a3425d8af544901e68291b26fdd1d504e
jishnu <jishnuu160@gmail.com>
2026-07-18 16:22:21 +0530

    added route for createOrder function

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,7 +1,8 @@
-import { verifyPayment,paymentFailed } from '../controllers/paymentController.js';
+import { verifyPayment,paymentFailed,createOrder } from '../controllers/paymentController.js';
 import express from 'express';
 const router = express.Router();
 
+router.post('/create-order', createOrder);
 router.post('/verify-payment', verifyPayment);
 router.get('/payment-failed', paymentFailed);
 export default router;
