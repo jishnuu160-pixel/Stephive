a6ba2bf59f3824a10bb4ed5b35663f1a6aa163d2
jishnu <jishnuu160@gmail.com>
2026-08-03 17:11:40 +0530

    added route for retry payment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,8 +1,9 @@
-import { verifyPayment,paymentFailed,createOrder } from '../controllers/paymentController.js';
+import { verifyPayment,paymentFailed,createOrder,retryPayment } from '../controllers/paymentController.js';
 import express from 'express';
 const router = express.Router();
 
 router.post('/create-order', createOrder);
 router.post('/verify-payment', verifyPayment);
 router.get('/payment-failed', paymentFailed);
+router.post('/retry', retryPayment);
 export default router;
