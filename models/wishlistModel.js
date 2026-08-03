dcf10ad0bf55108d0f90ab60e402740192244d9b
jishnu <jishnuu160@gmail.com>
2026-08-03 16:59:39 +0530

    added variantId and size

--- models/wishlistModel.js
+++ models/wishlistModel.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 const wishlistSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     items: [{
-        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true }
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+        variantId: { type: mongoose.Schema.Types.ObjectId, required: true },
+        size: { type: String, required: true }
     }]
 });
 
