c12d74ad46f71730f8a72c914f3af18aa48557ab
jishnu <jishnuu160@gmail.com>
2026-08-03 17:12:21 +0530

    removed unecessary routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,10 @@
 import express from 'express';
 const router= express.Router();
 
-import{getShop,getMenShopPage, getWomenShopPage, getProductId} from '../controllers/productController.js';
+import{getShop, getProductId} from '../controllers/productController.js';
 
 
 router.get('/shop', getShop);
-router.get('/shop/men', getMenShopPage);
-router.get('/shop/women', getWomenShopPage);
-
 
 router.get('/details/:id', getProductId);
 
