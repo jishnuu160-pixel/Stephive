1090ecd90e896b78bd34d7770a0e242ba90b8467
jishnu <jishnuu160@gmail.com>
2026-08-25 01:57:55 +0530

    Add fallback port 5500

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import {connectDB} from './config/db.js';
 import 'dotenv/config'; 
 
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 5500;
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
