3f0128deaf5d0cfdf8a27f7ac5932173a5a91553
jishnu <jishnuu160@gmail.com>
2026-08-24 23:46:24 +0530

    renamed userModel

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport';
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../models/usermodel.js';
+import User from '../models/userModel.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
