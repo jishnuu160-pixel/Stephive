c701506b94c6650f0a4cefd3110aa15cde579c75
jishnu <jishnuu160@gmail.com>
2026-08-24 23:45:42 +0530

    added the mongodburl to .env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/stephive');
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('Database connected');
     } catch (err) {
         console.log(err);
